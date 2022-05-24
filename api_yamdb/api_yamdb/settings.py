0f42b9f33c92435e67497c043825dbc5e7d10ab6
Алексей Бахмутов <ququ.kuku@yandex.ru>
2022-05-24 21:25:32 +0500

    9.9.7

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -98,7 +98,7 @@ AUTH_USER_MODEL = 'reviews.User'
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
