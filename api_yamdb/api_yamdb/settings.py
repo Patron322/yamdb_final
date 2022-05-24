ee5aba4004341d6ae71e0a37801f3882aef8d62f
Алексей Бахмутов <ququ.kuku@yandex.ru>
2022-05-24 21:39:35 +0500

    9.9.9

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = ['51.250.109.255', 'localhost', '127.0.0.1', 'web']
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
